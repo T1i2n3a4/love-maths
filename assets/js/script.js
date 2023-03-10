a8933d4d243e49068fd5663fd504c75df24a06f7
Cristina Stati <cristinastati1990@gmail.com>
2023-03-10 18:48:28 +0000

    Add event listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let buttons of button) {
+        button.addEventListener("click", function() {
+            if (this.getAttibute("data-type") ==="submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttibute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
+
 function addGame() {
 
 }
@@ -27,5 +43,5 @@ function displaySubtractQuestion() {
 }
 
 function displayMultiplyQuestion() {
-    
+
 }
